fd7f9e4030c8ea37632bfaba2965f6c934a72e7a
Khalil Gharbaoui <kaygeee@gmail.com>
2023-09-20 02:09:54 +0200

    Add configuration module/class

---
+++ lib/ruby_bol_api/configuration.rb
@@ -0,0 +1,14 @@
+module RubyBolAPI
+  class Configuration
+    attr_accessor :seller_client_id, :seller_client_secret, :base_url,
+                  :api_version, :logger
+
+    def initialize
+      @seller_client_id = nil
+      @seller_client_secret = nil
+      @base_url = base_url || "https://api.bol.com"
+      @api_version = api_version || "v9"
+      @logger = logger || Logger.new($stdout)
+    end
+  end
+end
